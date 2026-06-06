09ff835bef4300734668a631f18370dbc02989a9
shaikjahangeer01 <shaikjhangeerbasha@gmail.com>
2026-06-06 23:22:30 +0530

    Added home route

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const githubRoutes = require("./routes/githubRoutes");
 
 app.use("/api/github", githubRoutes);
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "GitHub Profile Analyzer API Running"
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
